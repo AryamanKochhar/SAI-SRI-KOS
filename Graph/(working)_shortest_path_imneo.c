229929d9767fc68c0a05b716ea7ee5b54f7eca13
XumoVoidHD <vedanshsahay05@gmail.com>
2024-11-05 05:48:41 +0530

    cyclic

--- Graph/(working)_shortest_path_imneo.c
+++ Graph/(working)_shortest_path_imneo.c
@@ -5,11 +5,10 @@
 
 #define MAX_VERTICES 7
 
-// Adjacency matrix representation of the graph
 int adj[MAX_VERTICES][MAX_VERTICES];
 
-// Function to perform BFS and find the shortest path
-int bfs(int start, int goal, int n, int* path) {
+int bfs(int start, int goal, int n, int *path)
+{
     int visited[MAX_VERTICES] = {0};
     int queue[MAX_VERTICES];
     int front = 0, rear = 0;
@@ -18,20 +17,24 @@ int bfs(int start, int goal, int n, int* path) {
     visited[start] = 1;
     queue[rear++] = start;
 
-    while (front != rear) {
+    while (front != rear)
+    {
         int curr = queue[front++];
-        if (curr == goal) {
-            // Reconstruct the path
+        if (curr == goal)
+        {
             int i = goal, idx = 0;
-            while (i != -1) {
+            while (i != -1)
+            {
                 path[idx++] = i;
                 i = parent[i];
             }
             return idx;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (adj[curr][i] && !visited[i]) {
+        for (int i = 0; i < n; i++)
+        {
+            if (adj[curr][i] && !visited[i])
+            {
                 visited[i] = 1;
                 parent[i] = curr;
                 queue[rear++] = i;
@@ -39,32 +42,36 @@ int bfs(int start, int goal, int n, int* path) {
         }
     }
 
-    return -1; // Path not found
+    return -1;
 }
 
-int main() {
+int main()
+{
     int n, start, goal;
     scanf("%d", &n);
 
-    // Read the adjacency matrix
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             scanf("%d", &adj[i][j]);
         }
     }
 
-    // Read the source and goal vertices
     scanf("%d %d", &start, &goal);
 
-    // Perform BFS and find the shortest path
     int path[MAX_VERTICES];
     int length = bfs(start, goal, n, path);
 
-    if (length == -1) {
+    if (length == -1)
+    {
         printf("Not found\n");
-    } else {
+    }
+    else
+    {
         printf("%d\n", length - 1);
-        for (int i = length - 1; i >= 0; i--) {
+        for (int i = length - 1; i >= 0; i--)
+        {
             printf("%d ", path[i]);
         }
         printf("\n");
