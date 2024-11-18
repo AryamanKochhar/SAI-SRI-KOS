adb91ec42b32138731874ca945ab75bcdfc23382
XumoVoidHD <vedanshsahay05@gmail.com>
2024-11-19 01:14:48 +0530

    owow

---
+++ FAT/circular_queue.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 5
+
+typedef struct
+{
+    int items[MAX];
+    int front, rear;
+} CircularQueue;
+
+void initializeQueue(CircularQueue *q)
+{
+    q->front = -1;
+    q->rear = -1;
+}
+
+int isEmpty(CircularQueue *q)
+{
+    return q->front == -1;
+}
+
+int isFull(CircularQueue *q)
+{
+    return (q->rear + 1) % MAX == q->front;
+}
+
+void enqueue(CircularQueue *q, int item)
+{
+    if (isFull(q))
+    {
+        printf("Queue is full. Cannot enqueue %d\n", item);
+        return;
+    }
+    if (isEmpty(q))
+    {
+        q->front = 0;
+    }
+    q->rear = (q->rear + 1) % MAX;
+    q->items[q->rear] = item;
+    printf("%d enqueued to queue\n", item);
+}
+
+int dequeue(CircularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty. Cannot dequeue\n");
+        return -1;
+    }
+    int item = q->items[q->front];
+    if (q->front == q->rear)
+    {
+        q->front = q->rear = -1;
+    }
+    else
+    {
+        q->front = (q->front + 1) % MAX;
+    }
+    return item;
+}
+
+int peek(CircularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty. Cannot peek\n");
+        return -1;
+    }
+    return q->items[q->front];
+}
+
+void displayQueue(CircularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue elements: ");
+    int i = q->front;
+    while (1)
+    {
+        printf("%d ", q->items[i]);
+        if (i == q->rear)
+            break;
+        i = (i + 1) % MAX;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    CircularQueue q;
+    initializeQueue(&q);
+
+    enqueue(&q, 10);
+    enqueue(&q, 20);
+    enqueue(&q, 30);
+    enqueue(&q, 40);
+    enqueue(&q, 50);
+
+    displayQueue(&q);
+
+    printf("Peek at front item: %d\n", peek(&q));
+    printf("%d dequeued from queue\n", dequeue(&q));
+    printf("%d dequeued from queue\n", dequeue(&q));
+
+    displayQueue(&q);
+
+    enqueue(&q, 60);
+    enqueue(&q, 70);
+
+    displayQueue(&q);
+
+    printf("Peek at front item: %d\n", peek(&q));
+
+    return 0;
+}
