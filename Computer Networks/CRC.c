4a8d94153b0a77410ed6761298a5d47c5c36299c
XumoVoidHD <vedanshsahay05@gmail.com>
2024-11-21 19:19:38 +0530

    CRC

--- Computer Networks/CRC.c
+++ Computer Networks/CRC.c
@@ -1,65 +1,88 @@
 #include <stdio.h>
-char div[20], temp[4], total[100];
-char data[20];
-int i, j, datalen, divlen, len, flag = 1;
-void check();
-int main()
-{
-    printf("Enter the total bit of data:");
-    scanf("%d", &datalen);
-    printf("\nEnter the total bit of divisor:");
-    scanf("%d", &divlen);
-    len = datalen + divlen - 1;
-    printf("\nEnter the data:");
-    scanf("%s", data);
-    printf("\nEnter the divisor:");
-    scanf("%s", div);
-    for (i = 0; i < datalen; i++)
-    {
-        total[i] = data[i];
-        temp[i] = data[i];
+#include <string.h>
+
+void computeCRC(char *data, char *div, int datalen, int divlen, char *remainder) {
+    int i, j;
+    char temp[20];
+
+    // Copy data to a temporary buffer and append zeros
+    strcpy(temp, data);
+    for (i = 0; i < divlen - 1; i++) {
+        temp[datalen + i] = '0';
     }
-    for (i = datalen; i < len; i++)
-        total[i] = '0';
-    check();
-    for (i = 0; i < divlen; i++)
-    {
-        temp[i + datalen] = data[i];
-    }
-    printf("\ntransmitted Code Word:%s", temp);
-    printf("\n\nEnter the received code word:");
-    scanf("%s", total);
-    check();
-    for (i = 0; i < divlen - 1; i++)
-        if (data[i] == '1')
-        {
-            flag = 0;
-            break;
-        }
-    if (flag == 1)
-    {
-        printf("\nSuccessful..!!\n\n");
-    }
-    else
-    {
-        printf("\nreceived code word contains errors...\n");
-    }
-}
-void check()
-{
-    for (j = 0; j < divlen; j++)
-        data[j] = total[j];
-    while (j <= len)
-    {
-        if (data[0] == '1')
-            for (i = 1; i < divlen; i++)
-            {
-                data[i] = ((data[i] == div[i]) ? '0' : '1');
+    temp[datalen + divlen - 1] = '\0';
+
+    // Perform division using XOR
+    for (i = 0; i < datalen; i++) {
+        if (temp[i] == '1') {
+            for (j = 0; j < divlen; j++) {
+                temp[i + j] = (temp[i + j] == div[j]) ? '0' : '1';
             }
-        for (i = 0; i < divlen - 1; i++)
-        {
-            data[i] = data[i + 1];
-            data[i] = total[j++];
         }
     }
+
+    // Extract the remainder
+    strncpy(remainder, temp + datalen, divlen - 1);
+    remainder[divlen - 1] = '\0';
+}
+
+int checkCRC(char *received, char *div, int len, int divlen) {
+    int i, j;
+    char temp[20];
+
+    strcpy(temp, received);
+
+    // Perform division using XOR
+    for (i = 0; i < len - divlen + 1; i++) {
+        if (temp[i] == '1') {
+            for (j = 0; j < divlen; j++) {
+                temp[i + j] = (temp[i + j] == div[j]) ? '0' : '1';
+            }
+        }
+    }
+
+    // Check if remainder is all zeros
+    for (i = len - divlen + 1; i < len; i++) {
+        if (temp[i] != '0') {
+            return 0; // Error detected
+        }
+    }
+    return 1; // No error
+}
+
+int main() {
+    int datalen, divlen;
+    char data[20], div[20], remainder[20], transmitted[40];
+
+    printf("Enter the total bit of data: ");
+    scanf("%d", &datalen);
+    printf("Enter the total bit of divisor: ");
+    scanf("%d", &divlen);
+
+    printf("Enter the data: ");
+    scanf("%s", data);
+    printf("Enter the divisor: ");
+    scanf("%s", div);
+
+    computeCRC(data, div, datalen, divlen, remainder);
+
+    printf("CRC: %s\n", remainder);
+
+    // Append the remainder to the data
+    strcpy(transmitted, data);
+    strcat(transmitted, remainder);
+    printf("Transmitted Code Word: %s\n", transmitted);
+
+    // Simulate reception
+    char received[40];
+    printf("\nEnter the received code word: ");
+    scanf("%s", received);
+
+    if (checkCRC(received, div, strlen(received), divlen)) {
+        printf("\nSuccessful..!!\n\n");
+    } else {
+        printf("\nReceived code word contains errors...\n");
+    }
+
+    return 0;
 }
