031eb06b81d2e4dc5242c5d77d25975ee92986d5
XumoVoidHD <vedanshsahay05@gmail.com>
2024-11-21 20:02:19 +0530

    parity

--- Computer Networks/parityBit.c
+++ Computer Networks/parityBit.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int calculateParityBit(unsigned char data)
+// Function to calculate the parity bit based on binary division by 2
+int calculateParityBit(const char *binary)
 {
-    int parity = 0;
-    for (int i = 0; i < 8; i++)
+    int count = 0;
+    int len = strlen(binary);
+
+    // Loop through the binary string from left to right
+    for (int i = 0; i < len; i++)
     {
-        parity ^= (data >> i) & 1;
+        if (binary[i] == '1')  // If the bit is '1', increment the count
+        {
+            count++;
+        }
     }
-    return parity;
+
+    // Return the parity bit based on whether the count is even or odd
+    return (count % 2 == 0) ? 0 : 1;
 }
 
-unsigned char binaryToByte(const char *binary)
-{
-    unsigned char result = 0;
-    for (int i = 0; i < 8 && binary[i] != '\0'; i++)
-    {
-        result = (result << 1) | (binary[i] - '0');
-    }
-    return result;
-}
-
+// Function to get valid binary input from the user
 void getBinaryInput(char *buffer, const char *prompt)
 {
     int valid = 0;
@@ -28,28 +29,30 @@ void getBinaryInput(char *buffer, const char *prompt)
     {
         printf("%s", prompt);
         scanf("%s", buffer);
-        valid = (strlen(buffer) == 8 && strspn(buffer, "01") == 8);
+        valid = (strspn(buffer, "01") == strlen(buffer));  // Check if input is binary
         if (!valid)
         {
-            printf("Invalid input. Please enter 8-bit binary data.\n");
+            printf("Invalid input. Please enter binary data consisting of only 0s and 1s.\n");
         }
     }
 }
 
 int main()
 {
-    char input1[9], input2[9];
-    getBinaryInput(input1, "Enter the first 8-bit binary data: ");
-    unsigned char data1 = binaryToByte(input1);
-    int parity1 = calculateParityBit(data1);
+    char input1[100], input2[100];
+    
+    // Get the first binary input
+    getBinaryInput(input1, "Enter the first binary data: ");
+    int parity1 = calculateParityBit(input1);
     printf("Parity Bit for first data: %d\n", parity1);
 
-    getBinaryInput(input2, "Enter the second 8-bit binary data: ");
-    unsigned char data2 = binaryToByte(input2);
-    int parity2 = calculateParityBit(data2);
+    // Get the second binary input
+    getBinaryInput(input2, "Enter the second binary data: ");
+    int parity2 = calculateParityBit(input2);
     printf("Parity Bit for second data: %d\n", parity2);
 
-    if (data1 == data2 && parity1 == parity2)
+    // Compare the binary data and parity bits
+    if (strcmp(input1, input2) == 0 && parity1 == parity2)
     {
         printf("Transmission successful, data is correct.\n");
     }
